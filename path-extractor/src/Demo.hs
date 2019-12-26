ff013a2b97e84419528290fc2996cfa09dc674e6
Florian Kohrs <florian.kohrs@web.de>
2019-12-26 11:53:10 +0100

    Add demo file to extract paths from
    See issue #5

--- path-extractor/src/Demo.hs
+++ path-extractor/src/Demo.hs
@@ -1,7 +1,12 @@
 module Demo where
 
-id' :: a -> a
-id' a = a
+
+
+mathStuff' 0 = 99
+mathStuff' val = val + 1
+
+--const' :: a -> b -> a
+const' a b = a
 
 mathStuff :: Int -> Int
 mathStuff val = if val > 0 then val * val else val + 1
